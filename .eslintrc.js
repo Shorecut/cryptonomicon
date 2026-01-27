991e5867dc458c94e219daa0665f5f9ebd532211
SBolotov <SBolotov@beeline.kg>
2026-01-27 15:50:36 +0600

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,21 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
   ],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+    // отключаем все ошибки prettier
+    'prettier/prettier': 'off',
+
+    // отключаем ошибки по v-for и неиспользуемым переменным
+    'vue/no-unused-vars': 'off',
+    'vue/require-v-for-key': 'off',
   },
-};
+}
